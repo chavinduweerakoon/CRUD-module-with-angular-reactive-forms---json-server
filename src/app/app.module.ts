37e7c55d86cba1b979d12b7cc730c3e0378812a1
chavindu weerakoon <chavinduweerakoon@gmail.com>
2021-07-31 21:46:03 +0530

    added a service for api calls and added http client module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
@@ -7,7 +8,7 @@ import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashbo
 
 @NgModule({
   declarations: [AppComponent, EmployeeDashboardComponent],
-  imports: [BrowserModule, ReactiveFormsModule],
+  imports: [BrowserModule, ReactiveFormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
