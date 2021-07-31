f1bb6ea5ec7cad83e372c89473a77ad370d1469e
chavindu weerakoon <chavinduweerakoon@gmail.com>
2021-07-31 21:29:24 +0530

    added reactive form module and data bindings in the form

--- src/app/employee-dashboard/employee-dashboard.component.ts
+++ src/app/employee-dashboard/employee-dashboard.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-employee-dashboard',
   templateUrl: './employee-dashboard.component.html',
-  styleUrls: ['./employee-dashboard.component.css']
+  styleUrls: ['./employee-dashboard.component.css'],
 })
 export class EmployeeDashboardComponent implements OnInit {
+  formValue!: FormGroup;
 
-  constructor() { }
+  constructor(private formbuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.formValue = this.formbuilder.group({
+      firstname: [''],
+      lastname: [''],
+      email: [''],
+      mobile: [''],
+      salary: [''],
+    });
   }
-
 }
