743e4ca1311f7f6eca850dd89d2abab49c2ae912
chavindu weerakoon <chavinduweerakoon@gmail.com>
2021-07-31 15:00:27 +0530

    added employee dashboard compenent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeDashboardComponent
   ],
   imports: [
     BrowserModule
