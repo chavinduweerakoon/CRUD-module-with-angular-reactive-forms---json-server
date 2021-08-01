25b4f9358de4c84d28371b786bf019197b4d5876
chavindu weerakoon <chavinduweerakoon@gmail.com>
2021-08-01 20:34:46 +0530

    added employee model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashboard.component';
+import { ApiService } from './shared/api.service';
 
 @NgModule({
   declarations: [AppComponent, EmployeeDashboardComponent],
-  imports: [BrowserModule, ReactiveFormsModule, HttpClientModule],
+  imports: [BrowserModule, ReactiveFormsModule, HttpClientModule, ApiService],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/employee-dashboard/employee-dashboard.component.ts
+++ src/app/employee-dashboard/employee-dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ApiService } from '../shared/api.service';
+import { EmployeeModel } from './employee-dashboard.model';
 
 @Component({
   selector: 'app-employee-dashboard',
@@ -8,8 +10,9 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class EmployeeDashboardComponent implements OnInit {
   formValue!: FormGroup;
+  employeeModelObj: EmployeeModel = new EmployeeModel();
 
-  constructor(private formbuilder: FormBuilder) {}
+  constructor(private formbuilder: FormBuilder, private api: ApiService) {}
 
   ngOnInit(): void {
     this.formValue = this.formbuilder.group({
@@ -19,5 +22,22 @@ export class EmployeeDashboardComponent implements OnInit {
       mobile: [''],
       salary: [''],
     });
+  }
+  postEmployeeDetails() {
+    this.employeeModelObj.firstName = this.formValue.value.firstName;
+    this.employeeModelObj.lastName = this.formValue.value.lastName;
+    this.employeeModelObj.email = this.formValue.value.email;
+    this.employeeModelObj.mobile = this.formValue.value.mobile;
+    this.employeeModelObj.salary = this.formValue.value.salary;
+
+    this.api.postEmployee(this.employeeModelObj).subscribe(
+      (res) => {
+        console.log(res);
+        alert('Employee added succcessfully');
+      },
+      (err) => {
+        alert('something went wrong');
+      }
+    );
   }
 }
