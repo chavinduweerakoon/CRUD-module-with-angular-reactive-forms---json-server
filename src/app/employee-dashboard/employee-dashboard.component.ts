e5641563d39894c942e94c3c9ff9478079abcfdf
chavindu weerakoon <chavinduweerakoon@gmail.com>
2021-08-01 22:06:34 +0530

    update records method

--- src/app/employee-dashboard/employee-dashboard.component.ts
+++ src/app/employee-dashboard/employee-dashboard.component.ts
@@ -62,10 +62,28 @@ export class EmployeeDashboardComponent implements OnInit {
   }
   //set feilds on edit
   onEdit(row: any) {
+    this.employeeModelObj.id = row.id;
     this.formValue.controls['firstName'].setValue(row.firstName);
     this.formValue.controls['lastName'].setValue(row.lastName);
     this.formValue.controls['email'].setValue(row.email);
     this.formValue.controls['mobile'].setValue(row.mobile);
     this.formValue.controls['salary'].setValue(row.salary);
+  }
+  //update
+  updateEmployeeDetails() {
+    this.employeeModelObj.firstName = this.formValue.value.firstName;
+    this.employeeModelObj.lastName = this.formValue.value.lastName;
+    this.employeeModelObj.email = this.formValue.value.email;
+    this.employeeModelObj.mobile = this.formValue.value.mobile;
+    this.employeeModelObj.salary = this.formValue.value.salary;
+
+    this.api
+      .updateEmployee(this.employeeModelObj, this.employeeModelObj.id)
+      .subscribe((res) => {
+        alert('Updated Successfully');
+        let ref = document.getElementById('cancel');
+        ref?.click();
+        this.getAllEmployee();
+      });
   }
 }
